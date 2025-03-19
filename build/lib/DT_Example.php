546342afc19c7f885130a655f1ef34260d312983
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-03-19 16:25:28 -0400

    spelling: ilen
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- build/lib/DT_Example.php
+++ build/lib/DT_Example.php
@@ -394,7 +394,7 @@ class DT_Example
 			$a = explode('|', $type);
 			$t = '';
 
-			for ( $i=0, $ien=count($a) ; $i<$ien ; $i++ ) {
+			for ( $i=0, $iLen=count($a) ; $i<$iLen ; $i++ ) {
 				$t .= $this->build_table( $a[$i] );
 			}
 
@@ -453,7 +453,7 @@ class DT_Example
 			}
 			else {
 				$cells = '';
-				for ( $i=0, $ien=count($columns) ; $i<$ien ; $i++ ) {
+				for ( $i=0, $iLen=count($columns) ; $i<$iLen ; $i++ ) {
 					$cells .= '<th>'.$this->_column( $columns[$i], 'title' ).'</th>';
 				}
 				$t .= '<thead>';
@@ -479,7 +479,7 @@ class DT_Example
 					}
 
 					$cells = '';
-					for ( $i=0, $ien=count($columns) ; $i<$ien ; $i++ ) {
+					for ( $i=0, $iLen=count($columns) ; $i<$iLen ; $i++ ) {
 						$cell = $this->_column( $columns[$i], 'data', $this->_data[$j] );
 
 						if ( strpos( $cell, '<td' ) === 0 ) {
@@ -508,7 +508,7 @@ class DT_Example
 			}
 			else {
 				$cells = '';
-				for ( $i=0, $ien=count($columns) ; $i<$ien ; $i++ ) {
+				for ( $i=0, $iLen=count($columns) ; $i<$iLen ; $i++ ) {
 					$cells .= '<th>'.$this->_column( $columns[$i], 'title' ).'</th>';
 				}
 				$t .= '<tfoot>';
@@ -558,7 +558,7 @@ class DT_Example
 		$out = array();
 		$libs = $this->_libs[ $type ];
 
-		for ( $i=0, $ien=count($libs) ; $i<$ien ; $i++ ) {
+		for ( $i=0, $iLen=count($libs) ; $i<$iLen ; $i++ ) {
 			$file = $libs[$i]; // needs a path
 
 			if ( strpos($file, '//') !== 0 &&
@@ -608,7 +608,7 @@ class DT_Example
 			if ( isset( $lib['lib'] ) ) {
 				$split_attr = explode( ' ', (string)$lib['lib'] );
 
-				for ( $i=0, $ien=count($split_attr) ; $i<$ien ; $i++ ) {
+				for ( $i=0, $iLen=count($split_attr) ; $i<$iLen ; $i++ ) {
 					$a[] = $split_attr[$i];
 					$this->_xml_libs[ $type ][] = $split_attr[$i];
 				}
@@ -624,7 +624,7 @@ class DT_Example
 		$exampleLibs = &$this->_libs[ $type ];
 		$srcLibs = DT_Example::$lookup_libraries[ $type ];
 
-		for ( $i=0, $ien=count($libs) ; $i<$ien ; $i++ ) {
+		for ( $i=0, $iLen=count($libs) ; $i<$iLen ; $i++ ) {
 			$lib = $libs[$i];
 
 			if ( strpos($lib, '/') === 0 || strpos($lib, '.') === 0 ) {
